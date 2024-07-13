4942d105d20bec6ca58b033fd0eff72781162851
Filipe Souto <filipesoutocosta@gmail.com>
2024-07-13 08:09:00 -0300

    practice JS

---
+++ Lessons/Lesson04/class08.js
@@ -0,0 +1,6 @@
+var spe = 60
+console.log(`The speed of your car is ${spe}`)
+if (spe > 60) { //Simple condition
+    console.log(`You've ultrapassed the allowed speed. Fined`)
+}
+console.log(`Always drive  using security belt`)
