68ea0806bbda27c329ca46f90536f5c22c2db9c5
Filipe Souto <filipesoutocosta@gmail.com>
2024-07-17 10:02:06 -0300

    updating

---
+++ Lessons/Lessontest/lessontest2/script.js
@@ -0,0 +1,22 @@
+    var checki = document.querySelector('input#check')
+    // var img = document.querySelector('img#person');
+    // var sex = document.querySelector('input#sex');
+    var ageS = document.querySelector('input#birth');
+    // var birth = Number(ageS.value);
+    var now = new Date();
+    var year = now.getFullYear();
+    // var par = document.querySelector('p.txt');
+    
+    checki.addEventListener('click', checka);
+
+    // var age = year - birth;
+
+    // par.innerHTML = `Here we see that you are: ${sex} and you are ${age} yo`;
+
+    // if () {
+
+    // }
+
+    function checka() {
+        
+    }
